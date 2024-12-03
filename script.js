3901b5dba6b1916607e44e7784b4cf9f03112b72
meegan29 <keckcamille2@gmail.com>
2024-12-04 01:25:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,32 +49,50 @@ Promise.all([d3.json("sgmap.json"), d3.csv("population2023.csv")]).then(data => 
             d3.select("#tooltip").style("display", "none");
         });
 
-    // Add a color legend for population size
-    let legendWidth = 200, legendHeight = 10;
-    let legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(800, 30)");
+     // Create a legend group
+     let legendWidth = 300, legendHeight = 20;
 
-    // Create a scale for the legend
-    let legendScale = d3.scaleLinear()
-        .domain([0, 126460])  // Population range from 0 to 126460
-        .range([0, legendWidth]);
-
-    // Add the colored rectangles for the legend
-    legend.append("g")
-        .attr("class", "legend-key")
-        .selectAll("rect")
-        .data(d3.range(0, 126460, 20000))  // Adjusting to display more or fewer keys
-        .enter().append("rect")
-        .attr("x", d => legendScale(d))
-        .attr("width", legendWidth / 10)
-        .attr("height", legendHeight)
-        .style("fill", d => colorScale(d));
-
-    // Add text labels to the legend
-    legend.append("text")
-        .attr("x", 0)
-        .attr("y", legendHeight + 20)
-        .text("Population");
-
-});
+     let legend = svg.append("g")
+         .attr("class", "legend")
+         .attr("transform", "translate(20, 20)");
+ 
+     // Create a linear scale for the legend axis
+     let legendScale = d3.scaleLinear()
+         .domain([d3.min(mapData, d => d.popdata), d3.max(mapData, d => d.popdata)])
+         .range([0, legendWidth]);
+ 
+     // Create gradient for the legend
+     let gradient = legend.append("defs")
+         .append("linearGradient")
+         .attr("id", "gradient")
+         .attr("x1", "0%")
+         .attr("y1", "0%")
+         .attr("x2", "100%")
+         .attr("y2", "0%");
+ 
+     // Create gradient stops based on color scale
+     gradient.selectAll("stop")
+         .data(d3.range(0, 1, 1 / 10).map(t => {
+             return {
+                 offset: `${t * 100}%`,
+                 color: colorScale(legendScale.invert(t * legendWidth))
+             };
+         }))
+         .enter().append("stop")
+         .attr("offset", d => d.offset)
+         .attr("stop-color", d => d.color);
+ 
+     // Add the gradient rectangle to the legend
+     legend.append("rect")
+         .attr("width", legendWidth)
+         .attr("height", legendHeight)
+         .style("fill", "url(#gradient)");
+ 
+     // Add the axis to the legend
+     legend.append("g")
+         .attr("transform", "translate(0," + legendHeight + ")")
+         .call(d3.axisBottom(legendScale)
+             .tickFormat(d3.format(".0s"))
+             .ticks(5));  // 5 ticks on the axis
+ 
+ });
